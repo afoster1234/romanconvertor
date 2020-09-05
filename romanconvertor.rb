7b7472f6ead7a69da6e317e6f280854f9b81a11e
Austin Foster <afoster@bsu.edu>
2020-09-04 23:10:40 -0400

    First Commit

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,47 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    r_map = { 1 => "I",
+    4 => "IV",
+    5 => "V",
+    9 => "IX",
+    10 => "X",
+    40 => "XL",
+    50 => "L",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M" }
+    num = 0
+    for key, value in r_map
+        while romanNumber.index(key) == 0
+            num += value
+            num.slice!(key)
+        end
+    end
+    num
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    r_map = { 1 => "I",
+    4 => "IV",
+    5 => "V",
+    9 => "IX",
+    10 => "X",
+    40 => "XL",
+    50 => "L",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M" }
+    num = 0
+    r_numerals = Array.new(3999) do |index|
+        num = index + 1
+        r_numerals.keys.sort { |a,b| b <=> a }.inject("") do |roman,div|
+            times, num = num.divmod(div)
+            roman << r_map[div] * times
+        end
+    end
 end
